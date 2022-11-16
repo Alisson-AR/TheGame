09e696e7378c63c92dfd5aa5c826bfb83b3c8084
unknown <thiagofrancisco627@gmail.com>
2022-11-15 22:15:40 -0300

    Removido musica tema do jogo devido ao tamnho muito grande do arquivo .wav, na proxima versao vamos analisar para utilizar um formato de musica (.mp3 possivelmente) que use muito espaço de disco

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,13 +1,10 @@
 
-import control.ControlaMusica;
 import control.Controlamenu;
 import model.Weapon;
 // Nomes: Thiago F. Santos, Anderson L. Almeida, Alisson Rosa
 
 public class GameMain {
 	public static void main(String[] args) {
-
-		ControlaMusica Musica = new ControlaMusica(System.getProperty("user.dir")+"\\src\\musicTheme\\MusicaTheGame.wav");
 
 		Controlamenu controlamenu = new Controlamenu();
 	}
